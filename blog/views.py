d16ad2bb5c66b77d60287ffa4a9de3783909e122
felki <felkamp@mail.ru>
2020-02-04 10:10:45 +0300

    add form for creating tags

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post, Tag
 from django.views.generic import View
 from django.shortcuts import get_object_or_404
 from .utils import ObjectDetailMixin
+from .forms import TagForm
 
 
 # Create your views here.
@@ -22,6 +23,20 @@ class TagDetail(ObjectDetailMixin, View):
     #     # tag = Tag.objects.get(slug__iexact=slug)
     #     tag = get_object_or_404(Tag, slug__iexact=slug)
     #     return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})
 
 
 def posts_list(request):
